73cb86f8d6e0670311960f4e3ea19af686b3b265
Tyler Erickson <tyler@vorgeo.com>
2026-01-26 16:36:29 -0800

    Add version method

--- src/iucn_get_data/__init__.py
+++ src/iucn_get_data/__init__.py
@@ -1,5 +1,8 @@
 """IUCN Get Data - Tools for working with IUCN Global Ecosystem Typology data."""
 
+from importlib.metadata import version
+
 from .main import get_realms, get_biomes, get_groups, get_typology
 
-__all__ = ['get_realms', 'get_biomes', 'get_groups', 'get_typology']
+__version__ = version("iucn-get-data")
+__all__ = ['get_realms', 'get_biomes', 'get_groups', 'get_typology', '__version__']
